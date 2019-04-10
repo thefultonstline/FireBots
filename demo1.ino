7859a48e6449ff238cba6df3aab00b4494bd7931
AJ Lahiri <al4566@nyu.edu>
2019-04-10 18:04:33 -0400

    Using an IR Distance Sensor

---
+++ demo1.ino
@@ -0,0 +1,15 @@
+//Demo for use of IR Distance sensor
+int adc;
+float voltage;
+
+void setup(){
+  pinMode(A0, INPUT);//sets up pin for input
+  Serial.begin(9600);//sets up serial communications for display
+}
+
+void loop(){
+  ADC = analogRead(A0);//read the analog voltage and convert to digital representation
+  voltage = ADC/1023.0;//convert digital ADC value to voltage
+  Serial.println(voltage);//display
+  delay(100);//wait 100 milliseconds
+}
