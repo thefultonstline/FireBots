3ac344ff596d5ae25205d6ffcaffa659a1945ab1
AJ Lahiri <al4566@nyu.edu>
2019-04-18 02:34:39 +0000

    Using an IR Sensor

--- demo1.ino
+++ demo1.ino
@@ -8,8 +8,8 @@ void setup(){
 }
 
 void loop(){
-  ADC = analogRead(A0);//read the analog voltage and convert to digital representation
-  voltage = (ADC/1023.0)*5.0;//convert digital ADC value to voltage
+  adc = analogRead(A0);//read the analog voltage and convert to digital representation
+  voltage = (adc/1023.0)*5.0;//convert digital ADC value to voltage
   Serial.println(voltage);//display
   delay(100);//wait 100 milliseconds
 }
